7947d205827b6ef66d972d5a8c1722dc3af054f7
32tesla32 <32tesla32@users.noreply.github.com>
2020-10-05 02:21:40 +0300

    2

--- src/Lesson_1/Main.java
+++ src/Lesson_1/Main.java
@@ -1,4 +1,22 @@
 package Lesson_1;
 
 public class Main {
+    public static void main(String[] args) {
+
+        byte b1 = 10; // -128 127
+        short s1 = 500; // -30k 30k
+        int i1 = 9000; // 2000000000
+        long l1 = 88888888L;
+
+        float f1 = 120.0f;
+        double d1 = 13.2;
+
+        boolean bool = true;
+        char c1 = 'b';
+
+        String str = "Привет";
+        System.out.println(b1 +" "+s1+" "+i1+" "+l1+" "+f1+" "+d1+" "+c1+" "+str);
+
+
+    }
 }
