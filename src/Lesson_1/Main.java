62b601e05419c93f276550f4d5c13dcf01eb86e5
32tesla32 <32tesla32@users.noreply.github.com>
2020-10-05 10:37:29 +0300

    урок 1

--- src/Lesson_1/Main.java
+++ src/Lesson_1/Main.java
@@ -14,9 +14,61 @@ public class Main {
         boolean bool = true;
         char c1 = 'b';
 
-        String str = "Привет";
-        System.out.println(b1 +" "+s1+" "+i1+" "+l1+" "+f1+" "+d1+" "+c1+" "+str);
+        String str = "Привет";;
+        System.out.println(b1 + " " + s1 + " " + i1 + " " + l1 + " " + f1 + " " + d1 + " " + c1 + " " + str);
+        info1();
+        info2();
+        info3();
+        info4();
+        info5("Дима");
+        info6();
+
+    }
+    static void info1() {
+        int a = 8;
+        int b = 2;
+        int c = 10;
+        int d = 5;
+        System.out.println(a * (b + (c / d)));
 
 
     }
-}
+    static void info2(){
+        int a = 8;
+        int b = 2;
+        boolean f = true;
+        boolean g = false;
+        System.out.println((a + b) >= 10 && (a + b) <= 20 ? f : g);
+    }
+    static  void info3(){
+        int z = -58;
+        System.out.println(z >= 0 ? "положительное" : "отрицательное");
+
+    }
+    static void info4() {
+        int z = -10;
+        if (z < 0) {
+            System.out.println(true);
+        } else {
+
+        }
+
+    }
+    static void info5(String name){
+        System.out.println("Привет " +  name + '!');
+    }
+    static void info6() {
+        int a = 400;
+        if (a %100==0 && a %400 ==0 ) {
+            System.out.println("Високосный");
+
+        }else if (a % 4 == 0 && a % 100 > 0) {
+            System.out.println("Високосный");
+
+        }else if (a % 100 == 0) {
+
+        }else {
+        }
+        }
+
+    }
